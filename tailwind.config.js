0825a131be5a6f3fe79a205b579e52fbf4244b80
jinhyuck cha <ckwlsgur20@gmail.com>
2024-01-18 10:31:49 +0900

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  screens: {
+    theme: {
+      mobile: "480px",
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: "#06371f",
+      },
+    },
+  },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5542F6",
+        highlight: "#eae8fb",
+        bgGray: "#fbfafd",
+      },
+    },
+  },
+  plugins: [],
+};
