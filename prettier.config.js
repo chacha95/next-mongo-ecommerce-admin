841024fefddc6c7520db3b8fef8f4bc1a18cfe0d
jinhyuck cha <ckwlsgur20@gmail.com>
2024-01-19 11:06:34 +0900

    update prettier and eslint

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   singleQuote: true,
   arrowParens: "always",
-  trailingComma: "none",
+  trailingComma: "all",
   printWidth: 100,
   tabWidth: 2,
   plugins: ["prettier-plugin-tailwindcss"],
